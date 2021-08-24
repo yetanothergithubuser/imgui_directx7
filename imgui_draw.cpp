db63e3aa9aa28fe69b35d3e141a1083a4266fb49
ocornut <omarcornut@gmail.com>
2021-08-24 18:59:10 +0200

    Shadows: Fix broken shadows due to zero-clear added in 9417acc2

--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -369,7 +369,7 @@ void ImGui::StyleColorsLight(ImGuiStyle* dst)
 // [SECTION] ImFontAtlasShadowTexConfig
 //-----------------------------------------------------------------------------
 
-ImFontAtlasShadowTexConfig::ImFontAtlasShadowTexConfig()
+void ImFontAtlasShadowTexConfig::SetupDefaults()
 {
     TexCornerSize = 16;
     TexEdgeSize = 1;
@@ -3139,6 +3139,7 @@ ImFontAtlas::ImFontAtlas()
     TexGlyphPadding = 1;
     PackIdMouseCursors = PackIdLines = -1;
     ShadowRectIds[0] = ShadowRectIds[1] = -1;
+    ShadowTexConfig.SetupDefaults();
 }
 
 ImFontAtlas::~ImFontAtlas()
